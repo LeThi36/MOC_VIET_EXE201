e5205039ef451620dae5f0e1e0073c387ffb89f1
Chun1503 <tranminhtrung022@gmail.com>
2025-05-20 13:11:49 +0700

    Cart and Add To Cart ( need to add ajax to not reload the page )

---
+++ MocViet/BussinessLayer/DTOs/Cart/CartDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.DTOs.Cart
+{
+    public class CartDto
+    {
+        public string Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public List<CartItemDto> Items { get; set; } = new();
+        public decimal Total => Items.Sum(item => item.Total);
+    }
+}
---
+++ MocViet/MocViet/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using BussinessLayer.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MocViet.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        // GET: /Cart/
+        public async Task<IActionResult> Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "1"; // giả lập nếu chưa login
+            var cart = await _cartService.GetCartAsync(userId);
+            return View(cart);
+        }
+
+        // POST: /Cart/Add
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(string productId)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "1";
+
+            await _cartService.AddToCartAsync(userId, productId, 1);
+
+         
+            return RedirectToAction("Index", "Product");
+        }
+
+
+        // POST: /Cart/Update
+        [HttpPost]
+        public async Task<IActionResult> Update(string cartItemId, int quantity)
+        {
+            await _cartService.UpdateCartItemAsync(cartItemId, quantity);
+            return RedirectToAction("Index");
+        }
+
+        // POST: /Cart/Delete
+        [HttpPost]
+        public async Task<IActionResult> Delete(string cartItemId)
+        {
+            await _cartService.RemoveCartItemAsync(cartItemId);
+            return RedirectToAction("Index");
+        }
+    }
+}
